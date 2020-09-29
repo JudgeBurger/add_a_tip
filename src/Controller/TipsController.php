a01e3a156a553fcb455e1097cbbabce69a3d1986
JudgeBurger <karli.damien@gmail.com>
2020-09-29 17:33:10 +0200

    Create CRUD tips

---
+++ src/Controller/TipsController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Tips;
+use App\Form\TipsType;
+use App\Repository\TipsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/tips")
+ */
+class TipsController extends AbstractController
+{
+    /**
+     * @Route("/", name="tips_index", methods={"GET"})
+     * @param TipsRepository $tipsRepository
+     * @return Response
+     */
+    public function index(TipsRepository $tipsRepository): Response
+    {
+        return $this->render('tips/index.html.twig', [
+            'tips' => $tipsRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="tips_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $tip = new Tips();
+        $form = $this->createForm(TipsType::class, $tip);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($tip);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('tips_index');
+        }
+
+        return $this->render('tips/new.html.twig', [
+            'tip' => $tip,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="tips_show", methods={"GET"})
+     * @param Tips $tip
+     * @return Response
+     */
+    public function show(Tips $tip): Response
+    {
+        return $this->render('tips/show.html.twig', [
+            'tip' => $tip,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="tips_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Tips $tip
+     * @return Response
+     */
+    public function edit(Request $request, Tips $tip): Response
+    {
+        $form = $this->createForm(TipsType::class, $tip);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('tips_index');
+        }
+
+        return $this->render('tips/edit.html.twig', [
+            'tip' => $tip,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="tips_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Tips $tip
+     * @return Response
+     */
+    public function delete(Request $request, Tips $tip): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$tip->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($tip);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('tips_index');
+    }
+}
---
+++ src/Form/TipsType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tips;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TipsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('language')
+            ->add('example')
+            ->add('picture')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Tips::class,
+        ]);
+    }
+}
