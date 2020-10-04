b30d67a861158df64cbfbb99a462197214a83990
Damien <karli.damien@gmail.com>
2020-10-04 18:23:55 +0200

    Create route for dashbord, add connection for Admin, add cancel button but transform this button in create an account button

--- src/Security/LoginFormAutenticathorAuthenticator.php
+++ src/Security/LoginFormAutenticathorAuthenticator.php
@@ -4,6 +4,7 @@ namespace App\Security;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
