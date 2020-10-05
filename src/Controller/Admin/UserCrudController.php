63c2cd0685ab2bc0417f71285bfce92fde824180
Damien <karli.damien@gmail.com>
2020-10-05 23:12:36 +0200

    Test on UserCrudController

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use phpDocumentor\Reflection\Types\Integer;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +17,18 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    /**
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IntegerField::new('id', 'ID')->onlyOnIndex(),
+            TextField::new('email'),
+            TextEditorField::new('roles'),
+            TextField::new('password'),
+//             Pour determiner des champs associés :
+//             AssociationField::new('user')
         ];
     }
     */
+
 }
