2c649a71420f5a3c813148fbc78a8eb44f6300ab
Damien <karli.damien@gmail.com>
2020-10-08 23:15:43 +0200

    Modification Route Controller

--- src/Controller/TipsController.php
+++ src/Controller/TipsController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/tips")
+ * @Route("/")
  */
 class TipsController extends AbstractController
 {
