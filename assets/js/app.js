c46d0157fe349c393e88e56d5165cfbca57f3eb5
Damien <karli.damien@gmail.com>
2020-10-08 23:17:05 +0200

    install fontawesome, add scss for button and card

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,7 @@ import '../scss/app.scss';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
