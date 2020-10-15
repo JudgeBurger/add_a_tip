d5a3132863d4bce4c64e0220f09d126576e03dad
JudgeBurger <karli.damien@gmail.com>
2020-10-15 17:01:04 +0200

    add field and required in tipsType, modify new tips form

--- src/Form/TipsType.php
+++ src/Form/TipsType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Tips;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
@@ -14,9 +16,15 @@ class TipsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('language')
-            ->add('description')
+            ->add('name', TextType::class, [
+                'required' => true,
+            ])
+            ->add('language', TextType::class, [
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
             ->add('picture')
         ;
     }
