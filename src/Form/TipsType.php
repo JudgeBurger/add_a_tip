05a9b89e568ee82b004be23549d9f1d180fe03a4
Damien <karli.damien@gmail.com>
2020-10-15 23:00:52 +0200

    change card style

--- src/Form/TipsType.php
+++ src/Form/TipsType.php
@@ -5,26 +5,18 @@ namespace App\Form;
 use App\Entity\Tips;
 use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichFileType;
+
 
 class TipsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class, [
-                'required' => true,
-            ])
-            ->add('language', TextType::class, [
-                'required' => true,
-            ])
-            ->add('description', TextareaType::class, [
-                'required' => false,
-            ])
+            ->add('name')
+            ->add('language')
+            ->add('description')
             ->add('picture')
         ;
     }
