24b4f2c17e3344a51b1a1fe4f5d7af9b628df666
Damien <karli.damien@gmail.com>
2020-10-18 23:11:14 +0200

    add textareatype in TipsType

--- src/Form/TipsType.php
+++ src/Form/TipsType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Tips;
 use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,9 +15,19 @@ class TipsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('language')
-            ->add('description')
+            ->add(
+                'name',
+            )
+            ->add(
+                'language',
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                [
+                    'attr' =>
+                        [ 'class' => 'col-6 row-6' ]
+            ])
             ->add('picture')
         ;
     }
