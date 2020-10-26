00b741a260fe3bee42d6f390c1945c6cd35e36a4
Damien <karli.damien@gmail.com>
2020-10-26 21:59:24 +0100

    install PHPCS Fixer, modify READme.md and start clipboard.js

--- src/Security/LoginFormAutenticathorAuthenticator.php
+++ src/Security/LoginFormAutenticathorAuthenticator.php
@@ -4,7 +4,6 @@ namespace App\Security;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
--- src/Service/MessagesFlash.php
+++ src/Service/MessagesFlash.php
@@ -6,16 +6,16 @@ class MessagesFlash
 {
     public function create(string $data): string
     {
-        return $data . 'à été ajouté a votre collection.';
+        return $data.'à été ajouté a votre collection.';
     }
 
     public function delete(string $data): string
     {
-        return $data . 'à été supprimé de votre collection.';
+        return $data.'à été supprimé de votre collection.';
     }
 
     public function update(string $data): string
     {
-        return $data . 'à bien été modifié.';
+        return $data.'à bien été modifié.';
     }
 }
