98963b6a2ef491ce4cd7ff76431df04eee3b5d42
JudgeBurger <karli.damien@gmail.com>
2020-11-06 14:10:37 +0100

    card Style

--- src/Form/LanguageType.php
+++ src/Form/LanguageType.php
@@ -2,33 +2,29 @@
 
 namespace App\Form;
 
+use App\Entity\Language;
 use App\Entity\Tips;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TipsType extends AbstractType
+class LanguageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name')
-            ->add('code')
-            ->add('description', TextareaType::class, [
-                'attr' => [
-                    'rows' => '8',
-                ],
-            ])
-            ->add('picture')
-            ->add('languages')
-        ;
+            ->add('tips', ChoiceType::class, [
+                'choices' => 'title',
+                'class' => Tips::class,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Tips::class,
+            'data_class' => Language::class,
         ]);
     }
 }
